a56c3d2412fe616603ff4d6744bb104d32a147f7
Wietse <wietse2007.3@gmail.com>
2025-03-12 15:24:45 +0100

    Uitwerking van de exercise

--- Card.php
+++ Card.php
@@ -7,6 +7,35 @@ class Card
 
     function __construct($suit, $value)
     {
-        var_dump($suit, $value);
+        $this->suit = $suit;
+        $this->value = $value;
+    }
+
+    function show(): string
+    {
+        $suits = [
+            "schoppen" => "\u{2660}",
+            "harten" => "\u{2665}",
+            "ruiten" => "\u{2666}",
+            "klaveren" => "\u{2663}"
+        ];
+
+        $values = [
+            'aas'   => 'A',
+            'twee' => 2,
+            'drie' => 3,
+            'vier' => 4,
+            'vijf' => 5,
+            'zes' => 6,
+            'zeven' => 7,
+            'acht' => 8,
+            'negen' => 9,
+            'tien' => 10,
+            'boer'  => 'B',
+            'vrouw' => 'V',
+            'heer'  => 'H'
+        ];
+
+        return $suits[$this->suit] . " " . $values[$this->value] . PHP_EOL;
     }
 }
--- index.php
+++ index.php
@@ -2,4 +2,9 @@
 
 require_once 'Card.php';
 
-$card = new Card('klaveren', 'boer');
+$card1 = new Card('klaveren', 'boer');
+$card2 = new Card('ruiten', 'boer');
+$card3 = new Card('ruiten', 'vijf');
+echo $card1->show();
+echo $card2->show();
+echo $card3->show();
